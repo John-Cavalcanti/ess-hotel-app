6806f9e02055df43c0165449a48074153d9147c1
João Victor Cavalcanti <jvictorao123@gmail.com>
2024-01-25 18:24:21 -0300

    server_cs funcional, falta adicionar os outros controllers e seus respctivos endpoints

--- server_cs/Program.cs
+++ server_cs/Program.cs
@@ -1,6 +1,27 @@
+using server_cs.src.controllers;
+using server_cs.src.models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// getting the models
+AtracoesController atr = new();
+
+// configuring endpoints
 app.MapGet("/", () => "Hello World!");
+
+// Endpoint atracoes
+var atracoesRoute = app.MapGroup("/atracoes");
+atracoesRoute.MapGet("/", () => 
+{
+    return atr.GetAllAtracoes();
+});
+
+atracoesRoute.MapGet("/{id}", (string id) => {
+
+    Atracao atracao = atr.GetAtracaoById(id);
+    Console.WriteLine(id, ' ',atracao);
+    return atracao;
+});
 
 app.Run();
